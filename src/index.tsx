7c2b2bb5c295aefdfe5c9c570101fbef19f1600f
Adam Towers <adamtowers@NYC-US00088-MBP.local>
2018-10-09 17:48:08 -0400

    Microsoft README tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// // import App from './App';
+// import Hello from './components/Hello';
+// import './index.css';
+
+import { createStore, Action } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+
+const store = createStore<StoreState, Action<any>, void, void>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
